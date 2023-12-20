dc4bb89d8653d6a426fe667dcb05dc104cc5f8c6
Niu <100760855+NiuHuangxiaozi@users.noreply.github.com>
2023-12-20 09:08:37 +0800

    Update run.sh

--- ResNet152_multinode_DDP_parallelism/run.sh
+++ ResNet152_multinode_DDP_parallelism/run.sh
@@ -2,4 +2,4 @@
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
 export NCCL_IB_DISABLE=1
 export NCCL_DEBUG=info
-torchrun --nnodes=2 --nproc_per_node=1 --node_rank=0 --master_addr= --master_port=29500  train.py --epochs=1
+torchrun --nnodes=2 --nproc_per_node=1 --node_rank=0 --master_addr= --master_port=29500  train.py --epochs=1 --data_path='./data'
